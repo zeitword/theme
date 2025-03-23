abe12150870e6a816af9fbcb48a296ee9c73e4c3
Aaron Mahlke <aaron.mahlke@gmail.com>
2025-03-23 13:39:51 +0100

    Update HelloWorld.vue

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const { zeitLayer } = useAppConfig()
+const { zeitLayer } = useAppConfig();
 </script>
 
 <template>
   <div class="bg-red-50">
-    <h1>Hello World!</h1>
+    <h1>Hello Worldius !</h1>
     <pre>{{ zeitLayer }}</pre>
   </div>
 </template>
