0e2c5c920f34e770dde7e9064480aee24e451f1d
Aaron Mahlke <aaron.mahlke@gmail.com>
2025-03-24 11:18:18 +0100

    Remove global import again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,14 +15,6 @@ export default defineNuxtConfig({
   imports: {
     dirs: ["types"],
   },
-  components: {
-    global: true,
-    dirs: [
-      {
-        path: "./components",
-      },
-    ],
-  },
   image: {
     provider: "vercel",
     dir: "public",
