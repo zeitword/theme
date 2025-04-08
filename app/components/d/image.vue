af998d2ebcaa5ed0490853fdd9ff435666a49ce9
Aaron Mahlke <aaron.mahlke@gmail.com>
2025-04-08 13:53:33 +0200

    Update image.vue

--- app/components/d/image.vue
+++ app/components/d/image.vue
@@ -8,39 +8,9 @@ type Props = {
   height?: number;
 };
 
-const {
-  src,
-  alt,
-  width = 1000,
-  height = 500,
-  position = { left: 50, top: 50 },
-} = defineProps<Props>();
+const { src, alt, width = 1000, height = 500 } = defineProps<Props>();
 </script>
 
 <template>
-  <NuxtImg
-    v-if="!hasError"
-    :width="width"
-    :height="height"
-    :src="src"
-    :alt="alt"
-    v-slot="{ src, isLoaded, imgAttrs }"
-    @error="hasError = true"
-  >
-    <img
-      v-if="isLoaded"
-      v-bind="imgAttrs"
-      :src="src"
-      @error="hasError = true"
-      @load="hasError = false"
-    />
-  </NuxtImg>
-  <div
-    v-else
-    class="bg-neutral-strong text-neutral-weak grid place-items-center size-full"
-  >
-    <div class="flex flex-col gap-2 items-center">
-      <ImageOff class="size-4 stroke-3" />
-    </div>
-  </div>
+  <NuxtImg :width="width" :height="height" :src="src" :alt="alt" />
 </template>
